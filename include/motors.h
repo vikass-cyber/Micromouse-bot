18ba52f12cd0e72044e89d0a89436addff90c878
VIKAS <atl.leader.vikas@gmail.com>
2026-08-13 18:36:00 +0530

    Add motor header

--- include/motors.h
+++ include/motors.h
@@ -1,1 +1,18 @@
+#ifndef MOTORS_H
+#define MOTORS_H
 
+#include <Arduino.h>
+#include "config.h"
+
+// Initialize motor driver pins
+void initMotors();
+
+// Set left and right motor speeds.
+// Range: -255 to +255
+// Positive = forward, negative = reverse.
+void setMotors(int leftSpeed, int rightSpeed);
+
+// Stop both motors and disable the driver.
+void stopMotors();
+
+#endif // MOTORS_H
